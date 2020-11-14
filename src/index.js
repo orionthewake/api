612588b1b0eafeecd945c37f6f594a449eca236b
Joe Howard <joe@razeware.com>
2020-11-14 16:33:31 -0500

    Basic web application with Express

--- src/index.js
+++ src/index.js
@@ -1,2 +1,13 @@
 // index.js
 // This is the main entry point of our application
+
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 4000;
+
+app.get('/', (req, res) => res.send('Hello World!!!'));
+
+app.listen(4000, () =>
+  console.log('Server running at http://localhost:${port}')
+);
