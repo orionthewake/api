8effb4f72bdb848678c8c8aa3ed077e5deef19cf
Joe Howard <joe@razeware.com>
2020-11-14 20:35:50 -0500

    A first GraphQL API

--- src/index.js
+++ src/index.js
@@ -7,17 +7,48 @@ const { ApolloServer, gql } = require('apollo-server-express');
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' }
+];
+
 //  Construct a scheme, using GraphQL's schema language
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note
+  }
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 // Provide resolver functions for our schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!'
+    hello: () => 'Hello world!!!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    }
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      let noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott'
+      };
+      notes.push(noteValue);
+      return noteValue;
+    }
   }
 };
 
