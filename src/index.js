568b2de21b7ed73912eeed2a6e454edc68567e86
Joseph Howard <joe.howard@mac.com>
2023-02-16 17:49:19 -0500

    Add Note query and mutation

--- src/index.js
+++ src/index.js
@@ -6,10 +6,28 @@ const { ApolloServer, gql } = require('apollo-server-express');
 // Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
+let notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Oh hey look, another note!', author: 'Riley Harrison' },
+];
+
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
+  type Note {
+    id: String!
+    content: String!
+    author: String!
+  }
+
   type Query {
-    hello: String
+    hello: String!
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
@@ -17,6 +35,21 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello world!',
+    notes: () => notes,
+    note: (parent, args) => {
+      return notes.find(note => note.id === args.id);
+    },
+  },
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'Adam Scott',
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
   },
 };
 
