7cbd881acff0d52adebb0ae207d281905df5e5fa
Joseph Howard <joe.howard@mac.com>
2023-02-16 14:23:00 -0500

    GraphQL hello world query and resolver

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 // index.js
 
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
+// Run the server on a port specified in our .env file or port 4000
 const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Web Server!');
-});
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for the schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const app = express();
+
+// Apollo Server setup
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// Apply the GraphQL middleware and set the path to /api
+server.applyMiddleware({ app, path: '/api' });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}!`);
+  console.log(
+    `GraphQL server running at http://localhost:${port}${server.graphqlPath}`
+  );
 });
